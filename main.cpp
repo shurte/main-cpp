8884788188e11e24b70a7c66ce711c18c3340c05
shurte <shurte1982@gmail.com>
2025-10-24 11:47:43 +0200

    Add SDL initialization to the main.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <hello.hpp>
 
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
 int main(int argc, char** argv) {
     Hello hello;
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Quit();
+
     return 0;
 }
