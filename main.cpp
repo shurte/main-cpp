e7204937ea3e37a5d1b6ea649f6fea6ecde4d965
shurte <shurte1982@gmail.com>
2025-10-27 09:31:59 +0100

    Add glew to the main.

--- main.cpp
+++ main.cpp
@@ -3,10 +3,29 @@
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
 
+#include <GL/glew.h>
+
 int main(int argc, char** argv) {
     Hello hello;
 
     SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Hello",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        600,
+        600,
+        SDL_WINDOW_OPENGL
+    );
+
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    glewInit();
+    SDL_GL_MakeCurrent(window, context);
+
+    if (window != nullptr) {
+        SDL_DestroyWindow(window);
+    }
 
     SDL_Quit();
 
