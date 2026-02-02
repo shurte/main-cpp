62ef5e0fd8669b0cc518e054ab36a1417cbe3a3a
Aleś Makaraŭ <shurte1982@gmail.com>
2026-02-02 17:42:41 +0100

    Add some comments to the Controller class.

--- Controller.hpp
+++ Controller.hpp
@@ -12,6 +12,9 @@ using namespace std::chrono;
 #define WINDOW_SIZE_H 1200
 #define WINDOW_SIZE_V 600
 
+/**
+ * The main controller that synchronizes the game logic and the UI.
+ */
 class Controller {
     public:
         Controller() 
